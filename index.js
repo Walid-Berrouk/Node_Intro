86cad0ca5c7f51304c63e012a7b0933fdcafe3e7
Walid Berrouk <jw_berrouk@esi.dz>
2022-09-30 04:50:53 +0100

    Adding packages type section 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //? 1 - Packages Types
 
+// Local Modules
 const { ticket, Student, startingFunction } = require('./packages_Types/packages');
 
 console.log(ticket);
@@ -8,5 +9,20 @@ const p = new Student("john fev", 18);
 p.greetings()
 
 startingFunction()
+
+// Core Modules
+
+const fs = require('fs');
+const path = require('path');
+
+// External Modules
+
+const uuid = require('uuid');
+
+// generate universsal ids, have different versions (like v4)
+console.log(uuid.v4());
+console.log(uuid.v4());
+console.log(uuid.v4());
+console.log(uuid.v4());
 
 //? 2 - Global Variables
