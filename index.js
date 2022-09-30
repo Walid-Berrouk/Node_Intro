d9fc83a310a3f337c7612ec752b5915296ea1806
Walid Berrouk <jw_berrouk@esi.dz>
2022-09-30 06:13:26 +0100

    Adding logging system

--- index.js
+++ index.js
@@ -27,10 +27,53 @@ console.log(uuid.v4());
 
 //? 2 - Global Variables
 
-//? 2 - Packages Demo
+//? 3 - Packages Demo
 
     // a - path
     // b - fs
     // c - os
     // d - url
     // e - events
+
+//? 4 - Packages Demo
+
+const Logger = require('./logging/logger');
+// const fs = require('fs');
+// const path = require('path');
+
+const logger = new Logger();
+
+// Create folder that contains file logs
+
+fs.mkdir(path.join(__dirname, '/logging', '/logs'), {}, err => {
+    if (err) throw err;
+    console.log('Folder Created !')
+});
+
+// Create file that contains logs
+
+fs.writeFile(path.join(__dirname, '/logging', '/logs', 'logs.txt'), '', err => {
+    if (err) throw err;
+    console.log('File written to !')
+})
+
+
+// Log in place
+
+logger.on('message', (data) => {
+    console.log('Called Listner: ', data)
+    fs.appendFile(path.join(__dirname, '/logging', '/logs', 'logs.txt'), `id :${data.id} has made message : ${data.message} \n`, err => {
+            if (err) throw err;
+            console.log('File appended to !')
+        })
+})
+
+logger.log('Hello World')
+logger.log('Hi')
+logger.log('Hello')
+
+// Log Elsewhere
+
+const writter = require('./logging/writter');
+
+writter("testWritter", "This is a test writing")
---
+++ logging/logger.js
@@ -0,0 +1,13 @@
+const EventEmitter = require('events');
+const uuid = require('uuid');
+
+class Logger extends EventEmitter {
+    log(message) {
+        // Call event
+        this.emit('message', {id: uuid.v4(), message})
+    }
+}
+
+
+module.exports = Logger;
+// Go to index
