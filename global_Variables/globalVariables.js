5671f5ec7a2e1a1a26f9cb27d124b9ccba02f638
Walid Berrouk <jw_berrouk@esi.dz>
2022-09-30 10:55:37 +0100

    Adding clearing in global variables

--- global_Variables/globalVariables.js
+++ global_Variables/globalVariables.js
@@ -17,3 +17,12 @@ setTimeout(() => printFunc("setTimeout"), 2000);
 
 // Now call above function after 2 seconds
 setInterval(() => printFunc("setInterval"), 2000);
+
+// Clearing Time out
+// setTimeout(printFunc, 3000)
+// const timeout = setTimeout(() => printFunc("setTimeout"), 3000)
+
+// const interval = setInterval(() => printFunc("setInterval"), 3000)
+
+// clearTimeout(timeout)
+// setTimeout(() => clearInterval(interval), 3*3000)
